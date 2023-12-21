f33269ef56f9c999c4b3ab609a959d489fbb8bdb
NNK-KALI <neerajkumar18971@gmail.com>
2023-12-21 21:34:54 +0530

    unchanged

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ proxies = Scraper().scrape(protocol=["https"])
 print(f"scraped proxies queue len : {proxies.qsize()}")
 tpc = ThreadedProxyChecker()
 apc = AsyncProxyChecker()
-# async_working_proxies = apc.runner(proxies, _timeout=60, limit=3)
-# print(f"[async] working proxies queue len : {async_working_proxies.qsize()}")
-threaded_working_proxies = tpc.checker(proxies, _timeout=5, _max_workers=2,  limit=2)
-print(f"[threaded] working proxies queue len : {threaded_working_proxies.qsize()}")
+async_working_proxies = apc.runner(proxies, _timeout=5, limit=60)
+print(f"[async] working proxies queue len : {async_working_proxies.qsize()}")
+# threaded_working_proxies = tpc.checker(proxies, _timeout=1, _max_workers=100,  limit=100)
+# print(f"[threaded] working proxies queue len : {threaded_working_proxies.qsize()}")
 
 
