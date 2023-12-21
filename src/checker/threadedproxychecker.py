4b0845d4f4494163a588975bc89d196631f48979
NNK-KALI <neerajkumar18971@gmail.com>
2023-12-21 21:33:54 +0530

    implemented limit functionality

--- src/checker/threadedproxychecker.py
+++ src/checker/threadedproxychecker.py
@@ -1,18 +1,15 @@
-import requests
-import queue
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
-
+import queue
+import concurrent.futures
+import requests
 
 class ThreadedProxyChecker:
-
     def __init__(self):
         """
         constructor for ProxyChecker class.
         """
-        self.lock = threading.Lock() # mutex lock to avoid race conditions
+        pass 
 
-    
     def checker(self, proxies_queue, _timeout=5, _max_workers=100, limit=5):
         """
         This function takes a list of proxies and checks them concurrently using ThreadPoolExecutor.
@@ -26,39 +23,27 @@ class ThreadedProxyChecker:
         :rtype queue
         """
         valid_proxies_queue = queue.Queue()
-        
-
+       
         def check_proxy(valid_proxies_queue, proxy, _timeout, limit):
             try:
                 response = requests.get(
                     "https://ipinfo.io/json", proxies={"http": proxy, "https": proxy}, timeout=_timeout
                 )
-                if int(response.status_code) == 200: 
+                if response.status_code == 200:
                     if valid_proxies_queue.qsize() < limit:
                         valid_proxies_queue.put(proxy)
-                        print(f"[proxychecker] [Threaded] found valid proxy: {proxy}")
-                        return True
+                        print(f"[proxychecker] [Threaded] Valid proxy found: {proxy}")
                     else:
-                        print("raising error")
-                        raise ValueError("shutdown")
+                        # print("shuting down")
+                        self.executor.shutdown(wait=False, cancel_futures=True)
             except Exception as e:
                 # print(f"[proxychecker] [Threaded] Error {e}")
                 pass
-            return False
-                
         futures = []
-        with ThreadPoolExecutor(max_workers=_max_workers) as executor:
-            while (not proxies_queue.empty()):
-                proxy = proxies_queue.get()
-                futures.append(executor.submit(check_proxy, valid_proxies_queue, proxy, _timeout, limit))
-            print(len(futures))
-            try:
-                for future in as_completed(futures):
-                    future.result()
-            except Exception as e:
-                print("caught value err exception", e)
-                if True:
-                    executor._threads.clear()
-                    concurrent.futures.thread._threads_queues.clear()
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=_max_workers)
+        while not proxies_queue.empty() and valid_proxies_queue.qsize() < limit:
+            proxy = proxies_queue.get()
+            futures.append(self.executor.submit(check_proxy, valid_proxies_queue, proxy, _timeout, limit))
+        self.executor.shutdown(wait=True)
         return valid_proxies_queue
 
